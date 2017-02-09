f4c5e14fef7f091dfb8c45fab57a887158dcda1e
eason lee <15311471692@163.com>
2017-02-09 23:10:31 +0800

    服务器

--- api/view.py
+++ api/view.py
@@ -21,6 +21,8 @@ def format_tweets(tweets):
             t.format_data()
             t.comments.sort(key=lambda c: c.created_time, reverse=True)
             t.comments = t.comments[:6]
+            print('transmit_count',t.transmit_count,type(t.transmit_count))
+            print('praise',t.praise,type(t.praise))
             t.transmit_count = len(t.transmit_count)
             t.praise = len(t.praise)
             if t.transmit == '0':
