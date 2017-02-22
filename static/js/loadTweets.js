f1d676c133808f24cb875548d1bc2555b79f3ae8
eason lee <15311471692@163.com>
2017-02-22 17:47:06 +0800

    修改添加tweet到页面bug

--- static/js/loadTweets.js
+++ static/js/loadTweets.js
@@ -36,12 +36,20 @@ var loadTemplate = function (r) {
         var t = tweets[i];
         if (t.transmit == '0') {
             var temp = insertTweet(t,user_id);
-            $('.my-connects').append(temp);
+            if (page == 'plaza'){
+                $('.my-connects').append(temp);
+            } else {
+                $('.my-connect').append(temp);
+            }
         } else {
             var data = t;
             var tweet = t.tweet;
             var temp = insertTransmit(data,tweet,user_id);
-            $('.my-connects').append(temp);
+            if (page == 'plaza'){
+                $('.my-connects').append(temp);
+            } else {
+                $('.my-connect').append(temp);
+            }
         }
     }
 };
